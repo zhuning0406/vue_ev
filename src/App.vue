ee9c7c1e445e3d817a03f3f35be749fc01680811
zhuning <zhuning123@163.com>
2024-01-06 20:57:36 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <el-button plain>朴素按钮</el-button>
+    <el-button type="primary" plain>主要按钮</el-button>
+    <el-button type="success" plain>成功按钮</el-button>
+    <el-button type="info" plain>信息按钮</el-button>
+    <el-button type="warning" plain>警告按钮</el-button>
+    <el-button type="danger" plain>危险按钮</el-button>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="less"></style>
---
+++ src/main.js
@@ -0,0 +1,25 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+
+import './assets/global.less'
+
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+import axios from 'axios'
+
+Vue.use(ElementUI)
+
+axios.defaults.baseURL = 'https://big-event-vue-api-t.itheima.net/'
+
+Vue.prototype.$axios = axios
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
